d001b76f3e216bc99c0e64f559a417b2f461b9d2
mojtaba rouhi <roohi.m.1364@gmail.com>
2023-01-01 18:43:46 +0330

    change redis

--- src/main/java/com/example/redislibrary/RedisLibraryApplication.java
+++ src/main/java/com/example/redislibrary/RedisLibraryApplication.java
@@ -1,13 +1,27 @@
 package com.example.redislibrary;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class RedisLibraryApplication {
-
-	public static void main(String[] args) {
+public class RedisLibraryApplication implements CommandLineRunner  {
+	@Autowired
+	private  ApplicationContext appContext;
+	public static void main(String[] args)  {
 		SpringApplication.run(RedisLibraryApplication.class, args);
 	}
 
+
+	@Override
+	public void run(String... args) throws Exception {
+		String[] beans = appContext.getBeanDefinitionNames();
+		for(String bean:beans){
+			System.out.println("Bean name: " + bean);
+			Object object = appContext.getBean(bean);
+			System.out.println( "Bean object:" + object);
+		}
+	}
 }
