0b30d14b54c6543277ea15dd71d48d22f00541e7
mojtaba rouhi <roohi.m.1364@gmail.com>
2023-01-01 16:53:29 +0330

    first commit

---
+++ src/main/java/com/example/redislibrary/RedisLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.redislibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisLibraryApplication.class, args);
+	}
+
+}
